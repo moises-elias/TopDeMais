d5b54e275cd8897abb27f08d693ef7c8bd1b9a51
MoisesElias123 <moisesfelipedapaz@gmail.com>
2025-04-29 00:07:06 -0300

    feat(headerHover): fecha o cabeçalho ao clicar nos botões e reativa hover após 0.8s e organiza funções de exibição do cabeçalho

--- js/headerHover.js
+++ js/headerHover.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header"); // Seleciona o cabeçalho
+const buttonsNav = document.querySelectorAll(".nav a"); // Seleciona os botões do cabeçalho
 
 // Função para verificar se o cabeçalho deve ser escondido
 function shouldHideHeader() {
@@ -19,13 +20,32 @@ function updateHeaderVisibility() {
 window.addEventListener("scroll", updateHeaderVisibility); // Atualiza a visibilidade toda vez que o scroll mudar
 updateHeaderVisibility(); // Verifica a visibilidade do cabeçalho logo após o carregamento da página
 
-// Mouse Hover do cabeçalho
-header.addEventListener("mouseover", function () {
+function showHeader() {
   header.classList.remove("headerClose"); // Mostra o cabeçalho quando o mouse passar por cima
-});
+}
+// Mouse Hover do cabeçalho
+header.addEventListener("mouseover", showHeader);
 
-header.addEventListener("mouseout", function () {
+function hideHeader() {
   if (shouldHideHeader()) {
     header.classList.add("headerClose"); // Esconde o cabeçalho quando o mouse sair, se necessário
   }
+}
+
+// Mouse Out do cabeçalho
+header.addEventListener("mouseout", hideHeader);
+
+// Fecha o cabeçalho ao clicar nos botões
+buttonsNav.forEach(button => {
+  button.addEventListener("click", () => {
+    // Desativa o hover do cabeçalho
+    header.removeEventListener("mouseover", showHeader);
+    hideHeader();
+
+    // Ativa o hover do cabeçalho depois de 0.8s
+    setTimeout(() => {
+      header.addEventListener("mouseover", showHeader);
+    }, 800);
+    hideHeader();
+  });
 });
