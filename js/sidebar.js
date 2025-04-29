929f3545082689dbd606fff82020621e71d57661
MoisesElias123 <moisesfelipedapaz@gmail.com>
2025-04-29 01:08:44 -0300

    feat(sidebar): adiciona transição de opacidade e ajusta estilo de navegação lateral

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,10 +4,14 @@ const navLogin = document.getElementById("login");
 
 function closeSideBar() {
   sidebar.style.width = "0%";
-  navLogin.style.visibility = "visible";
+  navLogin.style.opacity = "1";
+  setTimeout(() => {
+    sidebar.style.visibility = "hidden";
+  }, 50);
 }
 
 function openSideBar() {
+  sidebar.style.visibility = "visible";
   sidebar.style.width = "40%";
-  navLogin.style.visibility = "hidden";
+  navLogin.style.opacity = "0";
 }
