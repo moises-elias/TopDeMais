90379a7b046e947ca055e6720aa2b8a3318c3676
MoisesElias123 <moisesfelipedapaz@gmail.com>
2025-05-06 01:15:36 -0300

    feat(sidebar):  adiciona comportamento de fechamento da barra lateral ao clicar nos botões de navegação e ajusta pequeno bug de visibilidade

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,7 @@
 //Barra Lateral
 const sidebar = document.getElementById("sidebar");
 const navLogin = document.getElementById("login");
+const buttonsNavSidebar = Array.from(document.querySelectorAll(".navSideBar a"));
 
 function closeSideBar() {
   sidebar.style.width = "0%";
@@ -15,3 +16,10 @@ function openSideBar() {
   sidebar.style.width = "40%";
   navLogin.style.opacity = "0";
 }
+
+//Fechar barra lateral ao clicar fora dela
+buttonsNavSidebar.forEach(button => {
+  button.addEventListener("click", () => {
+    closeSideBar();
+  });
+});
